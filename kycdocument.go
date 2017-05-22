9d08fb275ad419fd6efc84d2105b095dfeaf66f4
Anton B <antonb@columbus-internet.com>
2017-05-22 23:20:53 +0700

    KYC compliance

---
+++ kycdocument.go
@@ -0,0 +1,166 @@
+package mango
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// KYCDocumentType represents type of KYC document
+type KYCDocumentType int
+
+// KYCDocumentType ...
+const (
+	IdentityProof KYCDocumentType = iota
+	RegistrationProof
+	ArticlesOfAssociation
+	ShareholderDeclaration
+	AddressProof
+)
+
+var kycDocumentTypes = map[KYCDocumentType]string{
+	IdentityProof:          "IDENTITY_PROOF",
+	RegistrationProof:      "REGISTRATION_PROOF",
+	ArticlesOfAssociation:  "ARTICLES_OF_ASSOCIATION",
+	ShareholderDeclaration: "SHAREHOLDER_DECLARATION",
+	AddressProof:           "ADDRESS_PROOF",
+}
+
+type KYCDocumentStatus int
+
+const (
+	CREATED KYCDocumentStatus = iota
+	VALIDATION_ASKED
+	VALIDATED
+	REFUSED
+)
+
+var kycDocumentStatuses = map[KYCDocumentStatus]string{
+	CREATED:          "CREATED",
+	VALIDATION_ASKED: "VALIDATION_ASKED",
+	VALIDATED:        "VALIDATED",
+	REFUSED:          "REFUSED",
+}
+
+// KYCDocument ...
+type KYCDocument struct {
+	ID                   string
+	CreationDate         int64
+	Tag                  string
+	UserID               string
+	Status               string
+	RefusedReasonMessage string
+	RefusedReasonType    string
+
+	service      *MangoPay
+	documentType KYCDocumentType
+}
+
+// KYCDocumentList ...
+type KYCDocumentList []*KYCDocument
+
+// NewKYCDocument creates KYC document object for user
+func (m *MangoPay) NewKYCDocument(user Consumer, kycDocumentType KYCDocumentType) (*KYCDocument, error) {
+	id := consumerId(user)
+	if id == "" {
+		return nil, errors.New("unable to create KYC document: empty user ID")
+	}
+	d := &KYCDocument{
+		UserID:       id,
+		documentType: kycDocumentType,
+	}
+	d.service = m
+
+	data := JsonObject{}
+	j, err := json.Marshal(d)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(j, &data); err != nil {
+		return nil, err
+	}
+
+	for _, field := range []string{"Id", "CreationDate", "Status", "RefusedReasonMessage", "RefusedReasonType"} {
+		data[field] = int(data[field].(float64))
+	}
+
+	doc, err := d.service.anyRequest(new(KYCDocument), actionCreateUserKYCDocument, data)
+	if err != nil {
+		return nil, err
+	}
+	serv := d.service
+	*d = *(doc.(*KYCDocument))
+	d.service = serv
+	return nil, nil
+}
+
+// KYCDocuments ...
+func (m *MangoPay) KYCDocuments(user Consumer) (KYCDocumentList, error) {
+	id := consumerId(user)
+	if id == "" {
+		return nil, errors.New("unable to get KYC documents list: empty user ID")
+	}
+
+	docList, err := m.anyRequest(new(KYCDocumentList), actionListUserKYCDocuments, JsonObject{"UserId": id})
+	if err != nil {
+		return nil, err
+	}
+	return *(docList.(*KYCDocumentList)), nil
+}
+
+// KYCDocument ...
+func (m *MangoPay) KYCDocument(id string) (*KYCDocument, error) {
+	d, err := m.anyRequest(new(KYCDocument), actionViewKYCDocument, JsonObject{"Id": id})
+	if err != nil {
+		return nil, err
+	}
+	return d.(*KYCDocument), nil
+}
+
+// AddPage ...
+func (d *KYCDocument) AddPage(pageData string) error {
+	data := JsonObject{}
+	j, err := json.Marshal(d)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(j, &data); err != nil {
+		return err
+	}
+
+	for _, field := range []string{"CreationDate", "Status", "RefusedReasonMessage", "RefusedReasonType"} {
+		data[field] = int(data[field].(float64))
+	}
+
+	data["File"] = pageData
+
+	_, err = d.service.anyRequest(new(interface{}), actionCreateUserKYCDocumentPage, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Submit ...
+func (d *KYCDocument) Submit(status KYCDocumentStatus) error {
+
+	d.Status = kycDocumentStatuses[status]
+
+	data := JsonObject{}
+	j, err := json.Marshal(d)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(j, &data); err != nil {
+		return err
+	}
+
+	for _, field := range []string{"CreationDate", "RefusedReasonMessage", "RefusedReasonType"} {
+		data[field] = int(data[field].(float64))
+	}
+
+	_, err = d.service.anyRequest(new(interface{}), actionSubmitUserKYCDocument, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
