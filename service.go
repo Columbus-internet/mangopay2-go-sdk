62e4cd32fa4e24cdf6283f565bbeb9f385f45fa9
Anton B <antonb@columbus-internet.com>
2017-07-25 19:34:06 +0700

    fixes

--- service.go
+++ service.go
@@ -141,9 +141,11 @@ func (s *MangoPay) request(ma mangoAction, data JsonObject) (*http.Response, err
 
 	if page, pageOk := data["page"]; pageOk {
 		if perPage, perPageOk := data["per_page"]; perPageOk {
-			path = fmt.Sprintf("%s?page=%s&per_page=%s", path, page, perPage)
+			if sort, sortOk := data["sort"]; sortOk {
+			path = fmt.Sprintf("%s?page=%d&per_page=%d&sort=%s", path, page.(int), perPage.(int), sort.(string))
 			delete(data, "page")
 			delete(data, "per_page")
+			delete(data, "sort")
 		}
 	}
 
